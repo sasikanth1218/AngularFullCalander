21d1c9c3e909162ccbf043dd112b10a3a3999b93
sasikanth1218 <sasikanth1218@gmail.com>
2015-07-16 11:39:12 -0700

    Revert "AngularJs Custom full calader"
    This reverts commit 11a80df4a2c20a481767d181f04a1438a9fcf465.

--- filters/filters.js
+++
@@ -1,8 +0,0 @@
-/* Filters */
-
-angular.module('cdi.filters', []).
-    filter('interpolate', ['version', function(version) {
-        return function(text) {
-            return String(text).replace(/\%VERSION\%/mg, version);
-        }
-    }]);
